6f937ff5ce23cbb31dbd207f0b93fa103946170e
Bryan Park <bryan-j-park@outlook.com>
2022-11-22 11:45:22 -1000

    created budget model and controller

--- flask_app/controllers/transactions_controller.py
+++ flask_app/controllers/transactions_controller.py
@@ -2,6 +2,7 @@ from flask_app import app
 from flask import render_template, redirect, request, session
 from flask_app.models.transaction_models import Transaction
 from flask_app.models.user_models import User
+from flask_app.models.budget_models import Budget
 
 #dashboard page, access once user is logged in
 @app.route('/dashboard')
@@ -40,3 +41,34 @@ def delete_transaction(num):
   }
   Transaction.delete_transaction(data)
   return redirect('/dashboard')
+
+# used to check if the user selected a category to sort by
+@app.route('/sort_by_category', methods = ['POST'])
+def sort_by_category():
+  if 'uid' not in session:
+    return redirect('/')
+  
+  session['category'] = request.form['category']
+  if session['category'] == '':
+    return redirect(request.referrer)
+  return redirect('/sortted')
+
+# displays sorted data by selected category
+@app.route('/sortted')
+def sortted():
+  if 'uid' not in session:
+    return redirect('/')
+  
+  user_id = {
+    'id': session['uid']
+  }
+
+  data = {
+    'id': session['uid'],
+    'category': session['category']
+  }
+  user = User.get_one_by_id(user_id)
+  transactions = Transaction.get_category(data)
+  sort_by = session['category']
+  session.pop('category')
+  return render_template('', user = user, transactions = transactions, sort_by = sort_by)
