8fa91f94321da9b15cbbb3d9d7057f4d75b71133
Bryan Park <bryan-j-park@outlook.com>
2022-11-21 18:16:04 -1000

    created username validation, __init__

--- flask_app/models/user_models.py
+++ flask_app/models/user_models.py
@@ -2,7 +2,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_bcrypt import bcrypt
 from flask_app import EMAIL_REGEX
-from flask_app.controllers.user_controller import bcrypt
+from flask_app.controllers.users_controller import bcrypt
 
 
 
@@ -15,16 +15,19 @@ class User:
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
+        self.username = data['username']
         self.email = data['email']
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+# adding user to the database 
     @classmethod
     def create(cls, data):
         query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW());"
         return connectToMySQL("Bundle").query_db(query,data)
 
+# getting user information by id
     @classmethod
     def get_one_by_id(cls,data):
         query = "SELECT * FROM users WHERE id = %(id)s;"
@@ -36,11 +39,10 @@ class User:
             print(results[0])
             return cls(results[0])
 
-
+# getting the information of a user by email
     @classmethod
     def get_one_by_email(cls,data):
         query = "SELECT * FROM users WHERE email = %(email)s;"
-        
         results = connectToMySQL("Bundle").query_db(query,data)
 
         if not results:
@@ -49,6 +51,16 @@ class User:
             print(results[0])
             return cls(results[0])
 
+# getting user information by username
+    @classmethod
+    def get_one_by_username(cls,data):
+        query = 'SELECT * FROM users WHERE username = %(username)s;'
+        results = connectToMySQL('Bundle').query_db(query, data)
+        if not results:
+            return False
+        return cls(results[0])
+
+# validating user information before sending to database
     @staticmethod
     def validate_user(data):
         is_valid = True
@@ -59,7 +71,16 @@ class User:
         
         if len(data['last_name']) < 2:
             flash("Last name must be at least 2 characters", "error_message_last_name")
+            is_valid = False
+
+        if len(data['username']) < 5:
+            flash("Username must be at least 5 characters", "error_message_username")
+            is_valid = False
         
+        if (User.get_one_by_username(data)):
+            flash('Username already taken', 'error_message_username')
+            is_valid = False
+
         if not EMAIL_REGEX.match(data['email']):
             flash("Invalid email!","error_message_email")
             is_valid = False
@@ -78,11 +99,12 @@ class User:
 
         return is_valid
 
+# validating when user tries to login
     @staticmethod
     def validate_login(data):
         is_valid = True
-        print(data['email'])
-        found_user = User.get_one_by_email(data)
+        print(data['username'])
+        found_user = User.get_one_by_username(data)
         if found_user:
             if not bcrypt.check_password_hash(found_user.password, data['password']):
                 is_valid = False
